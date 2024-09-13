2ad02a334aedca4d499e06644522e0da6093e5be
Bob <bob@justbetter.nl>
2024-09-13 13:21:34 +0200

    Improve quality by adding phpstan and added sales rules labels

---
+++ src/Model/Resolver/Quote/Data/SalesRuleLabel.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Rapidez\Compadre\Model\Resolver\Quote\Data;
+
+use Magento\Framework\GraphQl\Config\Element\Field;
+use Magento\Framework\GraphQl\Schema\Type\ResolveInfo;
+use Magento\Framework\GraphQl\Query\ResolverInterface;
+use Magento\SalesRule\Api\RuleRepositoryInterface;
+use Magento\SalesRule\Api\Data\RuleInterface;
+use Magento\Quote\Model\Quote\Item;
+
+class SalesRuleLabel implements ResolverInterface
+{
+    public function __construct(
+        protected RuleRepositoryInterface $ruleRepository
+    )
+    {}
+
+    public function resolve(Field $field, $context, ResolveInfo $info, array $value = null, array $args = null): array
+    {
+        /** @var Item */
+        $cartItem = $value['model'];
+
+        $labels = [];
+
+        foreach (explode(',', (string) $cartItem->getAppliedRuleIds()) as $ruleId) {
+            /** @var RuleInterface $rule */
+            $rule = $this->ruleRepository->getById((int) $ruleId);
+
+            $labels[] = $rule->getName();
+        }
+
+        return $labels;
+    }
+}
